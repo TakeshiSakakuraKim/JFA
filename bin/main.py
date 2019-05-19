e1d908a68a40673eadce64996ce30f1121cf481f
kazu seida <kazuseida@chihara.local>
2019-05-19 21:19:32 +0900

    FJA

--- bin/main.py
+++ bin/main.py
@@ -3,21 +3,21 @@ import sys, os
 import numpy as np
 from skimage import io
 
-predictor_path = os.getcwd() + "src/dat/shape_predictor_68_face_landmarks.dat"
+predictor_path = os.getcwd() + "/src/dat/shape_predictor_68_face_landmarks.dat"
 detector = dlib.get_frontal_face_detector()
 predictor = dlib.shape_predictor(predictor_path)
 
 def main():
 	argv = sys.argv
-	print(argv)
-	img = io.imread(argv[1])
+	img = io.imread('./src/' + argv[1])
 	dets = detector(img, 1)
 	for d in dets:
 		parts = predictor(img, d).parts()
 		if len(parts) > 0:
 			landmarks = np.array([[p.x, p.y] for p in predictor(img, dets[0]).parts()])
 			print(landmarks)
-	print("no face")
+		else :
+			print("no face")
     
 if __name__ == '__main__':
 	main()
